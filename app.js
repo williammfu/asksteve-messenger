00287f5248390502452513265cc0c136677b1774
William Fu <williamfu37@gmail.com>
2021-04-17 01:16:03 +0700

    replied message

--- app.js
+++ app.js
@@ -1,54 +1,27 @@
 // Load dependencies
 const express = require('express')
-const bodyParser = require('body-parser')
 const request = require('request')
+const bodyParser = require('body-parser')
 const path = require('path')
 
+// Load Routers
+const webhook = require('./src/routes/webhook.routes')
+
 const app = express()
-
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.listen((process.env.PORT || 3000))
 
-// Server frontpage
+app.use('/webhook', webhook)
+
+// App frontpage
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/static/index.html'))
 })
 
 // Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge'])
-    } else {
-        res.send('Invalid verify token')
-    }
-})
+app.use('/webhook', webhook)
 
-function sendMessage(recipientId, message) {
-  request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: {
-          recipient: {id: recipientId},
-          message: message,
-      }
-  }, function(error, response, body) {
-      if (error) {
-          console.log('Error sending message: ', error)
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error)
-      }
-  })
-}
-
-app.post('/webhook', function (req, res) {
-  var events = req.body.entry[0].messaging;
-  for (i = 0; i < events.length; i++) {
-      var event = events[i];
-      if (event.message && event.message.text) {
-          sendMessage(event.sender.id, {text: "Echo: " + event.message.text})
-      }
-  }
-  res.sendStatus(200)
+const port = process.env.PORT || 3000
+app.listen( port , () => {
+    console.log(`Server is running on port ${port}`)
 })
---
+++ src/controller/webhook.controller.js
@@ -0,0 +1,51 @@
+const simpleFetch = (req, res) => {
+  if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+    res.send(req.query['hub.challenge'])
+  } else {
+    res.send('Invalid verify token')
+  }
+}
+
+const sendRequest = (req, res) => {
+  var events = req.body.entry[0].messaging
+  console.log(req.body)
+  for (i = 0; i < events.length; i++) {
+    var event = events[i]
+    if (event.message && event.message.text) {
+      handleEvent(event)
+    }
+  }
+  res.sendStatus(200)
+}
+
+// Static functions
+function handleEvent(event) {
+  if(event.message.text.toLowerCase() == 'hi') {
+    sendMessage(event.sender.id, { text: event.message.text + " there!" })
+  } else {
+    sendMessage(event.sender.id, { text: "Other msg 2020-12-01" })
+  }
+}
+
+function sendMessage(recipientId, message) {
+  request({
+      url: 'https://graph.facebook.com/v10.0/me/messages',
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+          recipient: {id: recipientId},
+          message: message,
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending message: ', error)
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+      }
+  })
+}
+
+module.exports = {
+  simpleFetch,
+  sendRequest
+}
